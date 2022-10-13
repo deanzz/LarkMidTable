aeda7e99d63e4a12f43f06a9bf6674834721e26e
Dean.Zhang <dean.zhang@kongming-inc.com>
2022-10-13 19:27:31 +0800

    hard code path for docker

--- larkmidtable-web/larkmt-core/src/main/java/com/larkmt/core/log/JobFileAppender.java
+++ larkmidtable-web/larkmt-core/src/main/java/com/larkmt/core/log/JobFileAppender.java
@@ -33,7 +33,7 @@ public class JobFileAppender {
 	 * 	---/2017-12-25/821.log
 	 *
 	 */
-	private static String logBasePath = "/home/deanzhang/app/larkmt-admin/data";
+	private static String logBasePath = "/opt/larkmt/larkmt-admin/data";
 	private static String glueSrcPath = logBasePath.concat("/gluesource");
 	public static void initLogPath(String logPath){
 		logger.info("initLogPath: " + logPath);
